f2d54a386592bcd0a2e75d566cc810fce7aeee30
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2026-04-15 23:17:30 -0700

    Feature/day progress widget (#1)
    * feat: initial commit for DayProgress app
    
    * feat: add DayProgressWidget with hourly timeline updates
    
    ---------
    
    Co-authored-by: Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>

---
+++ DayProgress/DayProgress/DayProgress/DayProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayProgressApp.swift
+//  DayProgress
+//
+//  Created by Abdulkarim Mziya on 2026-04-15.
+//
+
+import SwiftUI
+
+@main
+struct DayProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
